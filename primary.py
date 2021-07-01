b724180a267296cd7d6d6bf85bbf884ca92f447f
Tahlia Jones <tahliaj047@gmail.com>
2021-07-01 10:33:07 -0700

    update

--- primary.py
+++ primary.py
@@ -1,5 +1,5 @@
-#author: <author here>
-# date: <date here>
+#author: <Tahlia Jones>
+# date: <July 1, 2021>
 
 # --------------- Section 1 --------------- #
 # ---------- Integers and Floats ---------- #
@@ -12,7 +12,9 @@
 #       2 - create a print statement that prints the sum of three numbers
 #       3 - create a print statement the prints the sum of two negative numbers
 
-
+print('the sum of 51 + 29 =', 51 + 29 )
+print('the sum of 34 + 45 + 21 =', 34 + 45 + 21)
+print('the sum of -83 + -24 =', -83 + -24)
 
 # subtraction
 #   instructions
@@ -20,7 +22,9 @@
 #       2 - create a print statement that prints the difference of three numbers
 #       3 - create a print statement the prints the difference of two negative numbers
 
-
+print('the difference of 73 - 58 =', 73 - 58)
+print('the difference of 101 - 15 - 65 =', 101 - 15 - 65)
+print('the difference of -99 - -49 =', -99 - -49)
 
 # multiplication and true division
 #   instructions
@@ -28,24 +32,27 @@
 #       2 - create a print statement that prints the dividend of two numbers
 #       3 - create a print statement that evaluates an operation using both multiplication and division
 
-
+print('the product of 25 * 7 =', 25 * 7)
+print('the dividend of 245 / 24 =', 245 / 24)
+print('24 * 35 / 12', 24 * 35 / 12)
 
 # floor division
 #   instructions
 #       1 - using floor division, print the dividend of two numbers.
 
-
+print('the integer dividend of 245 / 34 =', 245 // 34)
 
 # exponentiation
 #   instructions
 #       1 - using exponentiation, print the power of two numbers
 
-
+print('the power of 212 ^ 3 =', 212 ** 3)
 
 # modulus
 #   instructions
 #       1 - using modulus, print the remainder of two numbers
 
+print('the reminder of 368 / 56 =', 368 % 56)
 
 # --------------- Section 2 --------------- #
 # ---------- String Concatenation --------- #
@@ -56,14 +63,19 @@
 #       2 - print the concatenation of five animals you like
 #       3 - print the concatenation of each word in a phrase
 
-
+print('Tahlia' + 'Jones')
+print('dogs' + 'tigers' + 'foxes' + 'starfish' + 'seahorses')
+print('it is raining cats and dogs')
 
 # duplication
 #   instructions
 #       1 - print the duplpication of your first 5 times
 #       2 - print the duplication of a song you like 10 times
 
-
+print('baby snow tigers' * 5)
+print('wish I had it' * 10)
 # concatenation and duplpication
 #   instructions
 #       1 - print the concatenation of two strings duplicated 3 times each
+
+
